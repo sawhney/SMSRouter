e0ce0c8ca766bbc7fc18ba98a4e07d7feb217b46
Sahaj Sawhney <sahaj.sawhney@gmail.com>
2015-03-07 20:27:42 +0000

    random decryption nonsense

--- app/src/main/java/com/mubhack2015/smsrouter/SMSReceiver.java
+++ app/src/main/java/com/mubhack2015/smsrouter/SMSReceiver.java
@@ -10,6 +10,20 @@ import android.provider.Telephony;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import android.util.Base64;
+
 /**
  * Created by sahaj on 07/03/2015.
  */
@@ -22,8 +36,64 @@ public class SMSReceiver extends BroadcastReceiver {
         SmsMessage messages[] = Telephony.Sms.Intents.getMessagesFromIntent(intent);
 
         for (SmsMessage msg : messages) {
-            smsManager.sendTextMessage(msg.getOriginatingAddress(), null,
-                    msg.getDisplayMessageBody(), null, null);
+            if(msg.getDisplayMessageBody().startsWith("<SMSRouter>")) {
+                smsManager.sendTextMessage(msg.getOriginatingAddress(), null,
+                        msg.getDisplayMessageBody(), null, null);
+            }
         }
     }
+
+    public void decrypt(Context context, String message) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+//            PrivateKey key = loadPrivateKey(context.getResources().openRawResource());
+//            cipher.init(Cipher.DECRYPT_MODE, key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static PrivateKey loadPrivateKey(String fileName)
+            throws IOException, GeneralSecurityException {
+        PrivateKey key = null;
+        InputStream is = null;
+        try {
+            is = fileName.getClass().getResourceAsStream("/" + fileName);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder builder = new StringBuilder();
+            boolean inKey = false;
+            for (String line = br.readLine(); line != null; line = br.readLine()) {
+                if (!inKey) {
+                    if (line.startsWith("-----BEGIN ") &&
+                            line.endsWith(" PRIVATE KEY-----")) {
+                        inKey = true;
+                    }
+                    continue;
+                }
+                else {
+                    if (line.startsWith("-----END ") &&
+                            line.endsWith(" PRIVATE KEY-----")) {
+                        inKey = false;
+                        break;
+                    }
+                    builder.append(line);
+                }
+            }
+
+            byte[] encoded = Base64.decode(builder.toString(), Base64.DEFAULT);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            key = kf.generatePrivate(keySpec);
+        } finally {
+            closeSilent(is);
+        }
+        return key;
+    }
+
+    public static void closeSilent(final InputStream is) {
+        if (is == null) return;
+        try { is.close(); } catch (Exception ign) {}
+    }
+
 }
